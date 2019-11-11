fc6b4888897548addff371a93e2df5831ee9eb33
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-11-11 16:31:53 +0000

    updated app.py 04

--- exercises/04-Constructor-Params/app.py
+++ exercises/04-Constructor-Params/app.py
@@ -1,8 +1,11 @@
 class Person:
+    def __repr__(self):
+        return '{Name:'+' '+self.name+', Last Name:'+' '+str(self.last_name)+', Birth Date:' +' '+ str(self.birth_date) +'}'
     def __init__ (self,e,r,t) :
         self.name = e
         self.last_name = r
         self.birth_date = t
 mary = Person("Mary", "Dylan", "Oct 12, 2002")
 july = Person('July','Mccafee','Nov 12, 2002')
-print(dir(x))
+print(mary)
+print(july)
