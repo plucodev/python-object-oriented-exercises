9f6a54b90d39ea0881d46473aec55ac705505c8c
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-11-11 16:38:30 +0000

    fixed issues 04

--- exercises/04-Constructor-Params/app.py
+++ exercises/04-Constructor-Params/app.py
@@ -1,10 +1,13 @@
 class Person:
     def __repr__(self):
         return '{Name:'+' '+self.name+', Last Name:'+' '+str(self.last_name)+', Birth Date:' +' '+ str(self.birth_date) +'}'
-    def __init__ (self,e,r,t) :
-        self.name = e
-        self.last_name = r
-        self.birth_date = t
+    # CHANGE ONLY THE CONSTRUCTOR FUNCTION
+    def __init__ (self, name, last, date) :
+        self.name = "Bob"
+        self.last_name = "Dylan"
+        self.birth_date = "May 24, 1941"
+
+# DON'T CHANGE THE CODE BELOW
 mary = Person("Mary", "Dylan", "Oct 12, 2002")
 july = Person('July','Mccafee','Nov 12, 2002')
 print(mary)
